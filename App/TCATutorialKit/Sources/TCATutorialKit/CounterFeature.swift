0feb7a443ab6641aca2ecb9c840d9d18ed46fd89
penguinsan <penguinsan.pg@gmail.com>
2023-11-30 19:52:25 +0900

    CounterView の body を実装
    - 表示するカウントの値やボタンをタップした時の処理については、後で実装する

--- App/TCATutorialKit/Sources/TCATutorialKit/CounterFeature.swift
+++ App/TCATutorialKit/Sources/TCATutorialKit/CounterFeature.swift
@@ -41,7 +41,29 @@ struct CounterView: View {
     let store: StoreOf<CounterFeature>
 
     var body: some View {
-        EmptyView()
+        VStack {
+            Text("0")
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+
+            HStack {
+                Button("-") {
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+
+                Button("+") {
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+            }
+        }
     }
 }
 
