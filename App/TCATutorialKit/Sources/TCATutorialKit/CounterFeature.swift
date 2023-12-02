680f1973223b7e27e6af23bd00d14a31807222a1
penguinsan <penguinsan.pg@gmail.com>
2023-12-02 14:09:17 +0900

    View に追加した部分に対応する State と Action を追加

--- App/TCATutorialKit/Sources/TCATutorialKit/CounterFeature.swift
+++ App/TCATutorialKit/Sources/TCATutorialKit/CounterFeature.swift
@@ -14,10 +14,13 @@ struct CounterFeature {
 
     struct State: Equatable {
         var count = 0
+        var fact: String?
+        var isLoading = false
     }
 
     enum Action {
         case decrementButtonTapped
+        case factButtonTapped
         case incrementButtonTapped
     }
 
@@ -26,10 +29,17 @@ struct CounterFeature {
             switch action {
             case .decrementButtonTapped:
                 state.count -= 1
+                state.fact = nil
+                return .none
+
+            case .factButtonTapped:
+                state.fact = nil
+                state.isLoading = true
                 return .none
 
             case .incrementButtonTapped:
                 state.count += 1
+                state.fact = nil
                 return .none
             }
         }
