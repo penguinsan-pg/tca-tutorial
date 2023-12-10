9bd601023138a2e104a8fa558f9d58e7442fe702
penguinsan <penguinsan.pg@gmail.com>
2023-12-10 17:59:40 +0900

    CounterFeatureTests を作成

---
+++ App/TCATutorialKit/Tests/TCATutorialKitTests/CounterFeatureTests.swift
@@ -0,0 +1,18 @@
+//
+//  CounterFeatureTests.swift
+//  
+//  
+//  Created by penguinsan on 2023/12/10
+//  
+//
+
+import ComposableArchitecture
+import XCTest
+@testable import TCATutorialKit
+
+@MainActor
+final class CounterFeatureTests: XCTestCase {
+
+    func testCounter() async {
+    }
+}
