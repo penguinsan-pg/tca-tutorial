6adc5ddaf85d302ff9915553a5ec52c641c82648
penguinsan <penguinsan.pg@gmail.com>
2023-12-29 18:40:37 +0900

    タイマーの実装方法を Task.sleep() から continuousClock に変更
    - テストコードで Clock を外部から操作できるようにするため

--- App/TCATutorialKit/Sources/TCATutorialKit/CounterFeature.swift
+++ App/TCATutorialKit/Sources/TCATutorialKit/CounterFeature.swift
@@ -30,6 +30,8 @@ struct CounterFeature {
 
     enum CancelID { case timer }
 
+    @Dependency(\.continuousClock) var clock
+
     var body: some ReducerOf<Self> {
         Reduce { state, action in
             switch action {
@@ -67,8 +69,7 @@ struct CounterFeature {
                 state.isTimerRunning.toggle()
                 if state.isTimerRunning {
                     return .run { send in
-                        while true {
-                            try await Task.sleep(for: .seconds(1))
+                        for await _ in self.clock.timer(interval: .seconds(1)) {
                             await send(.timerTick)
                         }
                     }
