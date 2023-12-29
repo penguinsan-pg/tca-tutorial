5ba8e5fd36ad874ca52b124a2a7110167d716354
penguinsan <penguinsan.pg@gmail.com>
2023-12-29 19:43:25 +0900

    API レスポンスの結果をアサートするコードを修正
    - ダミーの API レスポンスを仕込んだため、タイムアウトの時間を設定する必要は無くなった

--- App/TCATutorialKit/Tests/TCATutorialKitTests/CounterFeatureTests.swift
+++ App/TCATutorialKit/Tests/TCATutorialKitTests/CounterFeatureTests.swift
@@ -57,9 +57,9 @@ final class CounterFeatureTests: XCTestCase {
         await store.send(.factButtonTapped) {
             $0.isLoading = true
         }
-        await store.receive(\.factResponse, timeout: .seconds(1)) {
+        await store.receive(\.factResponse) {
             $0.isLoading = false
-            $0.fact = "???"  // API レスポンスがわからない
+            $0.fact = "0 is a good number."
         }
     }
 }
