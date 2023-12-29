b7b8d6bd663b3149d153914dbe6b259165e4cf72
penguinsan <penguinsan.pg@gmail.com>
2023-12-29 19:01:30 +0900

    API レスポンスを受け取った時の Action をアサートするコードを追加
    - ただし、API レスポンスが予測不可能なため現時点ではテストを書くことができない

--- App/TCATutorialKit/Tests/TCATutorialKitTests/CounterFeatureTests.swift
+++ App/TCATutorialKit/Tests/TCATutorialKitTests/CounterFeatureTests.swift
@@ -55,5 +55,9 @@ final class CounterFeatureTests: XCTestCase {
         await store.send(.factButtonTapped) {
             $0.isLoading = true
         }
+        await store.receive(\.factResponse, timeout: .seconds(1)) {
+            $0.isLoading = false
+            $0.fact = "???"  // API レスポンスがわからない
+        }
     }
 }
