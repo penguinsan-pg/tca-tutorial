2dc1f8abf28ef43d8ef09045f1ec005644698861
penguinsan <penguinsan.pg@gmail.com>
2023-12-29 20:17:00 +0900

    AppFeature.swift を作成

---
+++ App/TCATutorialKit/Sources/TCATutorialKit/AppFeature.swift
@@ -0,0 +1,18 @@
+//
+//  AppFeature.swift
+//
+//  
+//  Created by penguinsan on 2023/12/29
+//  
+//
+
+import SwiftUI
+
+struct AppView: View {
+
+    var body: some View {
+        TabView {
+
+        }
+    }
+}
