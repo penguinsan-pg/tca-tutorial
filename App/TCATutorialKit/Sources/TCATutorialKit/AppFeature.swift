a7cf0a5801964c48d77dfdaa02b2da946570a29d
penguinsan <penguinsan.pg@gmail.com>
2024-01-07 18:02:37 +0900

    AppFeature に State と Action を追加

--- App/TCATutorialKit/Sources/TCATutorialKit/AppFeature.swift
+++ App/TCATutorialKit/Sources/TCATutorialKit/AppFeature.swift
@@ -12,6 +12,15 @@ import SwiftUI
 @Reducer
 struct AppFeature {
 
+    struct State {
+        var tab1 = CounterFeature.State()
+        var tab2 = CounterFeature.State()
+    }
+
+    enum Action {
+        case tab1(CounterFeature.Action)
+        case tab2(CounterFeature.Action)
+    }
 }
 
 struct AppView: View {
