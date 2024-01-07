af2fc924c67ae63103622ebff61f210406953303
penguinsan <penguinsan.pg@gmail.com>
2024-01-07 18:17:08 +0900

    AppFeatureTests を作成

---
+++ App/TCATutorialKit/Tests/TCATutorialKitTests/AppFeatureTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppFeatureTests.swift
+//  
+//  
+//  Created by penguinsan on 2024/01/07
+//  
+//
+
+import ComposableArchitecture
+import XCTest
+@testable import TCATutorialKit
+
+final class AppFeatureTests: XCTestCase {
+
+    func testIncrementInFirstTab() {
+    }
+}
