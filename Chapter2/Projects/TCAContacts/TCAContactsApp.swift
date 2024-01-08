376a588c91d02e6e67d5d74b3c2d0429b400cad0
penguinsan <penguinsan.pg@gmail.com>
2024-01-08 19:10:40 +0900

    Xcode Project を作成

---
+++ Chapter2/Projects/TCAContacts/TCAContactsApp.swift
@@ -0,0 +1,18 @@
+//
+//  TCAContactsApp.swift
+//  TCAContacts
+//  
+//  Created by penguinsan on 2024/01/08
+//  
+//
+
+import SwiftUI
+
+@main
+struct TCAContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
