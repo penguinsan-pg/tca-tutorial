bc952d0d62de2bb4cfbdb168d6cc7cb6479f7bec
penguinsan <penguinsan.pg@gmail.com>
2024-01-18 01:03:17 +0900

    AddContactView を作成

--- Chapter2/TCAContactsKit/Sources/TCAContactsKit/AddContactFeature.swift
+++ Chapter2/TCAContactsKit/Sources/TCAContactsKit/AddContactFeature.swift
@@ -7,7 +7,7 @@
 //
 
 import ComposableArchitecture
-import Foundation
+import SwiftUI
 
 @Reducer
 struct AddContactFeature {
@@ -34,6 +34,29 @@ struct AddContactFeature {
             case .setName(let name):
                 state.contact.name = name
                 return .none
+            }
+        }
+    }
+}
+
+struct AddContactView: View {
+
+    let store: StoreOf<AddContactFeature>
+
+    var body: some View {
+        WithViewStore(self.store, observe: { $0 }) { viewStore in
+            Form {
+                TextField("Name", text: viewStore.binding(get: \.contact.name, send: { .setName($0) }))
+                Button("Save") {
+                    viewStore.send(.saveButtonTapped)
+                }
+            }
+            .toolbar {
+                ToolbarItem {
+                    Button("Cancel") {
+                        viewStore.send(.cancelButtonTapped)
+                    }
+                }
             }
         }
     }
