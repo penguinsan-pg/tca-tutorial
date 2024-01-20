4008b967b25d3ee5b53c04a6297b3c9827a7a0ea
penguinsan <penguinsan.pg@gmail.com>
2024-01-20 15:44:58 +0900

    連絡先追加機能で保存ボタンがタップされた時の処理を実装
    - 連絡先一覧機能の連絡先一覧に連絡先を追加
    - 機能の連携を解除

--- Chapter2/TCAContactsKit/Sources/TCAContactsKit/ContactsFeature.swift
+++ Chapter2/TCAContactsKit/Sources/TCAContactsKit/ContactsFeature.swift
@@ -40,6 +40,14 @@ struct ContactsFeature {
                 state.addContact = nil
                 return .none
 
+            case .addContact(.presented(.saveButtonTapped)):
+                guard let contact = state.addContact?.contact else {
+                    return .none
+                }
+                state.contacts.append(contact)
+                state.addContact = nil
+                return .none
+
             case .addContact:
                 return .none
             }
