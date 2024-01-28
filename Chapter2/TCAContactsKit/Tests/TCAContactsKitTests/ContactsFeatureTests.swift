38a0bcd99312ae2ea6c2e49f2b5d03c3bbb62498
penguinsan <penguinsan.pg@gmail.com>
2024-01-28 18:46:47 +0900

    Save ボタンタップの Action を TestStore に送信
    - この Action によって即座に State が更新されることはないので、末尾のクロージャは不要

--- Chapter2/TCAContactsKit/Tests/TCAContactsKitTests/ContactsFeatureTests.swift
+++ Chapter2/TCAContactsKit/Tests/TCAContactsKitTests/ContactsFeatureTests.swift
@@ -30,5 +30,6 @@ final class ContactsFeatureTests: XCTestCase {
         await store.send(.destination(.presented(.addContact(.setName("Blob Jr."))))) {
             $0.$destination[case: \.addContact]?.contact.name = "Blob Jr."
         }
+        await store.send(.destination(.presented(.addContact(.saveButtonTapped))))
     }
 }
