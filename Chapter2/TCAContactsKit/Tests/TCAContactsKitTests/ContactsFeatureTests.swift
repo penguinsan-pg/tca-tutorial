23de6fa5356e6ae262b2ee7faa177507fd900b8e
penguinsan <penguinsan.pg@gmail.com>
2024-01-28 19:40:27 +0900

    ContactsFeature.State の初期値の段階で、いくつかの連絡先を追加しておく
    - 削除のテストケースを確認しやすくするため

--- Chapter2/TCAContactsKit/Tests/TCAContactsKitTests/ContactsFeatureTests.swift
+++ Chapter2/TCAContactsKit/Tests/TCAContactsKitTests/ContactsFeatureTests.swift
@@ -65,7 +65,14 @@ final class ContactsFeatureTests: XCTestCase {
     }
 
     func testDeleteContact() async {
-        let store = TestStore(initialState: ContactsFeature.State()) {
+        let store = TestStore(
+            initialState: ContactsFeature.State(
+                contacts: [
+                    Contact(id: UUID(0), name: "Blob"),
+                    Contact(id: UUID(1), name: "Blob Jr."),
+                ]
+            )
+        ) {
             ContactsFeature()
         }
     }
