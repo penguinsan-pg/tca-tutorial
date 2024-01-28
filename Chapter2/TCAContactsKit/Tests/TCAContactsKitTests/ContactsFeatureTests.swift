053cf864d5c49bf7a3045af020d32c9c533ed10b
penguinsan <penguinsan.pg@gmail.com>
2024-01-28 19:42:07 +0900

    削除ボタンタップの Action を TestStore に送信

--- Chapter2/TCAContactsKit/Tests/TCAContactsKitTests/ContactsFeatureTests.swift
+++ Chapter2/TCAContactsKit/Tests/TCAContactsKitTests/ContactsFeatureTests.swift
@@ -75,5 +75,8 @@ final class ContactsFeatureTests: XCTestCase {
         ) {
             ContactsFeature()
         }
+
+        await store.send(.deleteButtonTapped(id: UUID(1))) {
+        }
     }
 }
