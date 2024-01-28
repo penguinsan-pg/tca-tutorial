0ea9420489dece1b5f0d5395e9f255964511c099
penguinsan <penguinsan.pg@gmail.com>
2024-01-28 18:12:08 +0900

    ContactsFeatureTests を作成

---
+++ Chapter2/TCAContactsKit/Tests/TCAContactsKitTests/ContactsFeatureTests.swift
@@ -0,0 +1,18 @@
+//
+//  ContactsFeatureTests.swift
+//  
+//  
+//  Created by penguinsan on 2024/01/28
+//  
+//
+
+import ComposableArchitecture
+import XCTest
+@testable import TCAContactsKit
+
+@MainActor
+final class ContactsFeatureTests: XCTestCase {
+
+    func testAddFlow() async {
+    }
+}
