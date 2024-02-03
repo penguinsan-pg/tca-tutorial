f9341c8d00c3b4bd6b6e1d7662fbfbd1f6f62062
penguinsan <penguinsan.pg@gmail.com>
2024-02-03 16:37:53 +0900

    Action を受け取った時の Reducer の動作を実装
    - Delete ボタンをタップすると、削除確認アラートを表示する
    - アラートの削除ボタンをタップすると、削除の delegate action を送信し、画面を閉じる

--- Chapter2/TCAContactsKit/Sources/TCAContactsKit/ContactDetailFeature.swift
+++ Chapter2/TCAContactsKit/Sources/TCAContactsKit/ContactDetailFeature.swift
@@ -30,10 +30,39 @@ struct ContactDetailFeature {
         case deleteButtonTapped
     }
 
+    @Dependency(\.dismiss) var dismiss
+
     var body: some ReducerOf<Self> {
         Reduce { state, action in
             switch action {
+            case .alert(.presented(.confirmDeletion)):
+                return .run { send in
+                    await send(.delegate(.confirmDeletion))
+                    await self.dismiss()
+                }
+
+            case .alert:
+                return .none
+
+            case .delegate:
+                return .none
+
+            case .deleteButtonTapped:
+                state.alert = .confirmDeletion
+                return .none
             }
+        }
+        .ifLet(\.$alert, action: \.alert)
+    }
+}
+
+extension AlertState where Action == ContactDetailFeature.Action.Alert {
+
+    static let confirmDeletion = Self {
+        TextState("Are you sure?")
+    } actions: {
+        ButtonState(role: .destructive, action: .confirmDeletion) {
+            TextState("Delete")
         }
     }
 }
