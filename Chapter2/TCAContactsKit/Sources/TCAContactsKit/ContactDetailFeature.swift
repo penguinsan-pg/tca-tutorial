e8ea5c37944c4b027351d4d85833d7fd392609f1
penguinsan <penguinsan.pg@gmail.com>
2024-02-03 15:25:18 +0900

    ContactDetailFeature を作成

---
+++ Chapter2/TCAContactsKit/Sources/TCAContactsKit/ContactDetailFeature.swift
@@ -0,0 +1,14 @@
+//
+//  ContactDetailFeature.swift
+//
+//  
+//  Created by penguinsan on 2024/02/03
+//  
+//
+
+import ComposableArchitecture
+import Foundation
+
+@Reducer
+struct ContactDetailFeature {
+}
