a3b14e0777b45e2ce4748a53342e909aeedef352
penguinsan <penguinsan.pg@gmail.com>
2025-02-01 18:31:38 +0900

    Xcode Project を作成
    - 今回は Swift Package は作成せずに、素の Xcode Project を利用する

---
+++ SyncUps/SyncUps/SyncUpsApp.swift
@@ -0,0 +1,18 @@
+//
+//  SyncUpsApp.swift
+//  SyncUps
+//  
+//  Created by penguinsan on 2025/02/01
+//  
+//
+
+import SwiftUI
+
+@main
+struct SyncUpsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
